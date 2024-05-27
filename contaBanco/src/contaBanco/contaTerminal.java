f3b67c2368ac878159c83d5e4bb08a17a0f2af0a
Deryc2 <ffdkzin6@gmail.com>
2024-05-26 23:34:44 -0300

    Adicionando arquivos ao repo remoto

---
+++ contaBanco/src/contaBanco/contaTerminal.java
@@ -0,0 +1,30 @@
+package contaBanco;
+
+import java.util.Scanner;
+
+public class contaTerminal {
+	public static void main(String[] args) {
+		//variáveis para guardar os dados do cliente
+		int numero;
+		String agencia, nome;
+		double saldo;
+		
+		Scanner entraDados = new Scanner(System.in);
+		System.out.print("Por favor, digite o número da Agência! ");
+		agencia = entraDados.nextLine();
+		
+		System.out.print("Por favor, digite o número da Conta: ");
+		numero = entraDados.nextInt();
+		entraDados.nextLine();
+		
+		
+		System.out.print("Por favor, digite seu Nome: ");
+		nome = entraDados.nextLine();
+		
+		System.out.print("Digite seu saldo: ");
+		saldo = entraDados.nextDouble();
+		
+		System.out.printf("Olá %s! Obrigado por criar uma conta em nosso banco, sua agência é %s conta %d  e seu saldo R$ %.2f já está disponível para saque.",nome,agencia,numero,saldo);
+		
+	}
+}
